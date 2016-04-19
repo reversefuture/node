fd98ae34ef1d1614499425ba35d99524cdb271c4
reversefuture <1756342058@qq.com>
2016-04-19 23:19:10 +0800

    16-4-19

---
+++ global&fs/rfNoOpen.js
@@ -0,0 +1,15 @@
+﻿var fs = require("fs");
+var buf = new Buffer(1024);
+
+   fs.read("input.txt",buf, 0, buf.length, 0, function(err, bytes){
+      if (err){
+         console.log(err);
+      }
+      console.log(bytes + "  字节被读取");
+      
+      // 仅输出读取的字节
+      if(bytes > 0){
+         console.log(buf.slice(0, bytes).toString());
+      }
+
+});
---
+++ global&fs/wf - 副本.js
@@ -0,0 +1,17 @@
+﻿var fs = require("fs");
+
+console.log("准备append文件");
+fs.appendFile('input.txt', '我是通过append的文件内容！',  function(err) { 
+   if (err) {
+       return console.error(err);
+   }
+   console.log("数据append成功！");
+   console.log("--------我是分割线-------------")
+   console.log("读取append的数据！");
+   fs.readFile('input.txt', function (err, data) {
+      if (err) {
+         return console.error(err);
+      }
+      console.log("异步读取文件数据: " + data.toString());
+   });
+});
